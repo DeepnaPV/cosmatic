49da290c5298f088e4546113cf2c6e7872b42191
DeepnaPV <deepnapv111@gmail.com>
2024-10-06 22:44:19 +0530

    product grid page

--- cosmetic_shop/user/urls.py
+++ cosmetic_shop/user/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     path('uhome',views.main, name='main'),
+    path('filter/', views.filter_products, name='filter_products'),
+    path('product/<int:id>/', views.detail, name='product_detail'),
+
 ]
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- cosmetic_shop/user/views.py
+++ cosmetic_shop/user/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from inventory.models import products
 
 # Create your views here.
 
 
 def main(request):
     return render(request,'uhome.html')
+
+def filter_products(request):
+    category = request.GET.get('category', None)
+    if category:
+        filtered_products = products.objects.filter(category=category)
+    else:
+        filtered_products = products.objects.all()  
+
+    return render(request, 'filter_products.html', {'products': filtered_products})
+
+def detail(request, id):
+    try:
+        product = products.objects.get(id=id)
+    except products.DoesNotExist:
+        return redirect('allpro')  # Redirect to all products if the product is not found
+
+    return render(request, 'uprodetail.html', {'product': product})
